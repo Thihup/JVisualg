07a5442d1906dd75d1e4790a916b882e4e61bc5e
Thiago Henrique Hüpner <thihup@gmail.com>
2024-05-07 09:42:42 -0300

    Add missing dependencies

--- dev.thihup.jvisualg.ide/src/main/java/module-info.java
+++ dev.thihup.jvisualg.ide/src/main/java/module-info.java
@@ -2,12 +2,11 @@ module dev.thihup.jvisualg.ide {
     requires javafx.base;
     requires javafx.graphics;
     requires javafx.fxml;
+    requires javafx.controls;
     requires org.fxmisc.richtext;
     requires reactfx;
-    requires dev.thihup.jvisualg.lsp;
-    requires org.eclipse.lsp4j;
-    requires org.eclipse.lsp4j.jsonrpc;
-    requires javafx.controls;
+    requires transitive dev.thihup.jvisualg.lsp;
+
     exports dev.thihup.jvisualg.ide to javafx.graphics;
 
     opens dev.thihup.jvisualg.ide to javafx.fxml;
