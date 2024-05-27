263f98427567ed208ae647d34a6d33f3c4b46654
Thiago Henrique Hüpner <thihup@gmail.com>
2024-05-27 16:14:08 -0300

    Improve coverage of native configuration

--- dev.thihup.jvisualg.ide/src/main/java/module-info.java
+++ dev.thihup.jvisualg.ide/src/main/java/module-info.java
@@ -1,13 +1,14 @@
 module dev.thihup.jvisualg.ide {
-    requires dev.thihup.jvisualg.lsp;
+    requires java.desktop;
+
+    requires dev.thihup.jvisualg.frontend;
     requires dev.thihup.jvisualg.interpreter;
+
     requires org.fife.RSyntaxTextArea;
     requires com.formdev.flatlaf;
-    requires dev.thihup.jvisualg.frontend;
+
     requires org.antlr.antlr4.runtime;
     requires rsyntaxtextarea.antlr4.extension;
-    requires java.desktop;
-    requires org.graalvm.nativeimage;
 
     exports dev.thihup.jvisualg.ide to org.fife.RSyntaxTextArea;
 }
