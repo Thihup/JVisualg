64137dc0ab1afc32a52a69c5f15f2a0c8fd3c5ad
Thiago Henrique Hüpner <thihup@gmail.com>
2024-05-27 13:02:43 -0300

    Register upcall stubs

--- dev.thihup.jvisualg.ide/src/main/java/dev/thihup/jvisualg/ide/NativeImageFeature.java
+++ dev.thihup.jvisualg.ide/src/main/java/dev/thihup/jvisualg/ide/NativeImageFeature.java
@@ -4,6 +4,8 @@ import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.hosted.RuntimeForeignAccess;
 
 import java.lang.foreign.FunctionDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import static java.lang.foreign.ValueLayout.*;
 import static java.lang.foreign.ValueLayout.JAVA_INT;
@@ -23,6 +25,22 @@ public class NativeImageFeature implements Feature {
         RuntimeForeignAccess.registerForDowncall(FunctionDescriptor.of(ADDRESS, ADDRESS, ADDRESS, ADDRESS, ADDRESS, ADDRESS));
         RuntimeForeignAccess.registerForDowncall(FunctionDescriptor.ofVoid(JAVA_INT, JAVA_INT, JAVA_INT, JAVA_FLOAT, JAVA_FLOAT, JAVA_FLOAT, JAVA_INT, JAVA_INT, ADDRESS, ADDRESS));
         RuntimeForeignAccess.registerForDowncall(FunctionDescriptor.ofVoid(JAVA_FLOAT, ADDRESS, ADDRESS, ADDRESS, JAVA_INT, JAVA_INT, JAVA_INT, JAVA_INT, JAVA_INT, JAVA_FLOAT, JAVA_FLOAT, JAVA_INT, JAVA_INT, ADDRESS, ADDRESS));
+
+        try {
+            Method registerForUpcall = RuntimeForeignAccess.class.getDeclaredMethod("registerForUpcall", Object.class, Object[].class);
+
+            Object[] objects = new Object[0];
+            registerForUpcall.invoke(null, FunctionDescriptor.of(JAVA_INT, ADDRESS, ADDRESS, JAVA_INT, JAVA_INT, ADDRESS, ADDRESS), objects);
+            registerForUpcall.invoke(null, FunctionDescriptor.of(JAVA_INT, ADDRESS, ADDRESS, JAVA_INT, ADDRESS, ADDRESS), objects);
+            registerForUpcall.invoke(null, FunctionDescriptor.of(JAVA_INT, ADDRESS, ADDRESS, JAVA_INT, ADDRESS), objects);
+            registerForUpcall.invoke(null, FunctionDescriptor.of(JAVA_INT, ADDRESS, ADDRESS, JAVA_INT, ADDRESS), objects);
+            registerForUpcall.invoke(null, FunctionDescriptor.of(JAVA_INT, ADDRESS, ADDRESS, JAVA_INT, JAVA_INT, ADDRESS, ADDRESS, ADDRESS), objects);
+            registerForUpcall.invoke(null, FunctionDescriptor.of(JAVA_INT, JAVA_INT, ADDRESS), objects);
+            registerForUpcall.invoke(null, FunctionDescriptor.ofVoid(JAVA_INT, JAVA_INT, JAVA_INT, JAVA_FLOAT, JAVA_FLOAT, JAVA_FLOAT, JAVA_INT, JAVA_INT, ADDRESS, ADDRESS), objects);
+
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
