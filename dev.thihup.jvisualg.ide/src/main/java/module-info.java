7f6b309fcc11fea40cdc2f54b036301c95328293
Thiago Henrique Hüpner <thihup@gmail.com>
2024-06-15 09:58:44 -0300

    Improve IDE with Search/Replace

--- dev.thihup.jvisualg.ide/src/main/java/module-info.java
+++ dev.thihup.jvisualg.ide/src/main/java/module-info.java
@@ -1,14 +1,18 @@
 module dev.thihup.jvisualg.ide {
     requires java.desktop;
 
-    requires dev.thihup.jvisualg.frontend;
     requires dev.thihup.jvisualg.interpreter;
 
     requires org.fife.RSyntaxTextArea;
     requires com.formdev.flatlaf;
+    requires autocomplete;
+    requires org.eclipse.lsp4j;
+    requires rstaui;
 
-    requires org.antlr.antlr4.runtime;
     requires rsyntaxtextarea.antlr4.extension;
+    requires dev.thihup.jvisualg.lsp;
+    requires dev.thihup.jvisualg.frontend;
+    requires org.antlr.antlr4.runtime;
 
     exports dev.thihup.jvisualg.ide to org.fife.RSyntaxTextArea;
 }
